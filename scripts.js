fb1f461055cd73d8afb3e695e96032ec72d4ada8
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-08-07 22:57:18 +0400

    cell size changable

--- scripts.js
+++ scripts.js
@@ -1,26 +1,33 @@
 const gridEl = document.getElementById('grid')
-const cells = []
-const num = 16
-let width = 512/16;
+const buttonEl = document.getElementById('size-btn')
+let cells = []
+const width = 512
 
-let i = 0
-while( i < num * num ) {
-    cells.push(document.createElement("div"))
-    cells[i].style.height = `${width}px`
-    cells[i].style.width = `${width}px`
-    //cells[i].style.border = "1px solid black"
-    gridEl.append(cells[i])
-    i++
+createGrid(16)
+
+buttonEl.addEventListener("click", event => {
+    let temp = prompt("Enter size: 1-100")
+    createGrid(temp)    
+})
+
+function addEventListeners() {
+    cells.forEach( cell => {
+        cell.addEventListener( 'mouseover', event => 
+           cell.style.backgroundColor = 'red')
+    });
 }
 
-cells.forEach( cell => {
-    cell.addEventListener( 'mouseover', event => 
-       cell.style.backgroundColor = 'red')
-});
-
-
-
-
-
-
-console.log(gridEl)
+function createGrid( sideNum ) {
+    cells = []
+    let num = sideNum * sideNum
+    let size = width/sideNum
+    let i = 0
+    while( i < num ) {
+        cells.push(document.createElement("div"))
+        cells[i].style.height = `${size}px`
+        cells[i].style.width = `${size}px`
+        i++
+    }
+    gridEl.replaceChildren(...cells)
+    addEventListeners()
+}
