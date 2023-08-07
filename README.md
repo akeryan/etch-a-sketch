4e8ef5c531dd6d135a4b923ffaaa20430b21623e
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-08-07 17:45:32 +0400

    grid is drawn

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+"Etch-a-Sketch" project is part of "The Odin Project" curriculum. 
+Link: https://www.theodinproject.com/lessons/foundations-etch-a-sketch
--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-console.log('Aram')
+const gridEl = document.getElementById('grid')
+const cells = []
+const num = 16
+let width = 512/16;
+
+let i = 0
+while( i < num * num ) {
+    cells.push(document.createElement("div"))
+    cells[i].style.height = `${width}px`
+    cells[i].style.width = `${width}px`
+    cells[i].style.border = "1px solid black"
+    gridEl.append(cells[i])
+    i++
+}
+
+
+
+
+
+
+console.log(gridEl)
