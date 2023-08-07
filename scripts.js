a18af9c4fa1e39c3adedb066e0e8c69745e5e2db
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-08-07 20:01:14 +0400

    mouseover drawing works

--- scripts.js
+++ scripts.js
@@ -8,10 +8,15 @@ while( i < num * num ) {
     cells.push(document.createElement("div"))
     cells[i].style.height = `${width}px`
     cells[i].style.width = `${width}px`
-    cells[i].style.border = "1px solid black"
+    //cells[i].style.border = "1px solid black"
     gridEl.append(cells[i])
     i++
 }
+
+cells.forEach( cell => {
+    cell.addEventListener( 'mouseover', event => 
+       cell.style.backgroundColor = 'red')
+});
 
 
 
