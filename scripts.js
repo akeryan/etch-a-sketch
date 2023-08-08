2864532087d5c8f775cd80539699dc4d589cf351
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-08-08 18:42:30 +0400

    Controlls are added
    Color picker works
    Greed resizer works

--- scripts.js
+++ scripts.js
@@ -1,24 +1,45 @@
 const gridEl = document.getElementById('grid')
 const buttonEl = document.getElementById('size-btn')
+const sizeInputEl = document.getElementById('myRange')
+const sizeOutputEl = document.getElementById('value')
+const colorPickerEl = document.getElementById('color-picker')
+
+let currentColor = "#ff0000" // default color
+
+colorPickerEl.value = currentColor; // assigning default corlor to the color picker
+
 let cells = []
 const width = 512
 
 createGrid(16)
+
+// SLIDER SECTION ==========================================================
+
+sizeOutputEl.textContent = `${sizeInputEl.value} X ${sizeInputEl.value}`;
+
+sizeInputEl.addEventListener("input", event => {
+    createGrid( event.target.value )
+    sizeOutputEl.textContent = `${event.target.value} X ${event.target.value}`;
+  });
 
 buttonEl.addEventListener("click", event => {
     let temp = prompt("Enter size: 1-100")
     createGrid(temp)    
 })
 
+colorPickerEl.addEventListener("input", event => {
+    currentColor = event.target.value
+})
+
 function addEventListeners() {
     cells.forEach( cell => {
         cell.addEventListener( 'mouseover', event => 
-           cell.style.backgroundColor = 'red')
+           cell.style.backgroundColor = currentColor)
     });
 }
 
 function createGrid( sideNum ) {
-    cells = []
+    cells.length = 0
     let num = sideNum * sideNum
     let size = width/sideNum
     let i = 0
